431930c5b7a1855bc6023a763b4baea81d8129a5
Marimariana <nilomariana@gmail.com>
2020-09-04 19:07:31 -0300

    add a esos html

--- js/main.js
+++ js/main.js
@@ -55,8 +55,41 @@ $(".btn-ob1").click(function(){
 
 
 
+
 $(".btn-ob2").click(function(){
   $(this).toggleClass("btn-obc");
 });
 
 
+//Acordeon
+
+$(document).ready(function(){
+
+  $("#acordeon > ul > li ul").each(function(index, element) {
+    var count = $(element).find("li").length;
+    var content = "" + count + "";
+  });
+  $("#acordeon ul ul li:odd").addClass("odd");
+$("#acordeon ul ul li:even").addClass("even");
+$("#acordeon > ul > li > a").click(function() {
+  var checkElement = $(this).next();
+
+  $("#acordeon li").removeClass("active");
+  $(this).closest("li").addClass("active");
+
+  if (checkElement.is("ul") && checkElement.is(":visible")) {
+    $(this).closest("li").removeClass("active");
+    checkElement.slideUp("normal");
+  }
+  if (checkElement.is("ul") && !checkElement.is(":visible")) {
+    $("#acordeon ul ul:visible").slideUp("normal");
+    checkElement.slideDown("normal");
+  }
+
+  if ($(this).closest("li").find("ul").children().length == 0) {
+    return true;
+  } else {
+    return false;
+  }
+});
+  });
